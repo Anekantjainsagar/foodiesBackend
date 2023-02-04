fdc8db032b8483fb596b87d876ae9d51bf83f8da
Anekant <Anekantjainsagar@gmail.com>
2023-02-04 11:33:22 +0530

    Adding cors

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 
+app.use(cors());
 //Database connection
+dotenv.config({ path: `./config.env` });
 
-dotenv.config({path:`./config.env`});
-
-const port = process.env.PORT
-require('./db/conn')
+const port = process.env.PORT;
+require("./db/conn");
 // const User = require(`./model/userSchema`)
 
 app.use(express.json());
 app.use(require(`./Router/auth.js`));
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}....`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}....`);
+});
